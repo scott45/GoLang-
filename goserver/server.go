c504b7d3efcc01207fa5801b79ec93d38f01b8bd
Scott Businge <businge.scott@andela.com>
2018-11-05 20:26:42 +0300

    refactor code and project structure

--- goserver/server.go
+++ goserver/server.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, r.URL.Path[1:])
+    })
+
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
     
